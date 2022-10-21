38f89f2e8a412c12158068264d737a68a9102644
Maxwell Abrams <90845816+MacAbrams@users.noreply.github.com>
2022-10-21 16:53:44 +0000

    need to do negtive version,

--- main.s
+++ main.s
@@ -4,12 +4,13 @@ section .data
   ;output db "00000000000000000000000000"
   switchTest db "01234567",10,0
   n db 10,0
+  t db 9,0
   minus db 45,0
 
 ;;;;;;;;;;;;;;;;
 ;;  XORSHIFT  ;;
 ;;;;;;;;;;;;;;;;
-  state dd 5  ;;
+  state dd 2  ;;
 ;;;;;;;;;;;;;;;;
 
 section .bss
@@ -20,25 +21,32 @@ section .text
 global _start
 _start:
 
-  xor rcx,rcx
+  mov rcx,-10
   loop:
-    call xorShift
+    ;call xorShift
 
-    mov dword [input], eax
+    mov dword [input], ecx
     lea rsi, input
     lea rdi, output
-    mov rbx, 2
-    call intToString  
+    mov rbx, 10
+    call floatToString 
     lea rsi, output
     call printS
+
+    lea rsi, t
+    call printS
+
+    mov rax, rcx
+    call printF
+
     lea rsi, n
     call printS
 
 
 
     inc rcx
-    cmp rcx, 100
-    ja ending
+    cmp rcx, 10
+    je ending
     jmp loop
 
 
@@ -48,26 +56,144 @@ _start:
 
 jmp ending
 
+; float 32 bit 
+;   0|00000000|00000000000000000000000
+;takes rax value
+printF:
+  push rdx
+  mov rdx, rax
+  shr rdx, 31
+  cmp rdx, 1
+  ja negative
+    mov rdx, rax
+    shl rdx, 9
+    shr rdx, 9
+    mov dword [input], edx
+    push rsi
+    push rbx
+    push rdi
+    lea rsi, input
+    lea rdi, output
+    mov rbx, 10
+    call intToString
+    lea rsi, output
+    call printS
+    pop rsi
+    pop rbx
+    pop rdi
+  contNeg:
 
+  
 
+  jmp end
+negative:
+  mov rdx, rax
+  shl rdx, 9
+  shr rdx, 9
+    mov dword [input], edx
+    push rsi
+    push rbx
+    push rdi
+    lea rsi, input
+    lea rdi, output
+    mov rbx, 10
+    call iIntToString
+    lea rsi, output
+    call printS
+    pop rsi
+    pop rbx
+    pop rdi
+  
 
-
-
-
-
-
-xorShift:
-  push rcx
-  mov eax, [state]
-  shl ecx, 13 
-  xor eax, ecx;
-  shr ecx, 17 
-  xor eax, ecx;
-  shl ecx, 5 
-  xor eax, ecx;
-  mov dword [state], eax;
-  pop rcx
+  jmp contNeg
+end:
+pop rdx
 ret
+  
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;takes int in memory and stores a string          ;;
+;returns memory address                           ;;
+; src in rsi                                      ;;
+; base in rbx                                     ;;
+; dest in rdi                                     ;;
+floatToString:                                     ;;
+  push rax                                        ;;
+  push rcx    
+  push r10
+  ;;zero output(128 bytes)                        ;;
+  mov rax, 66                                     ;;
+  call zeroMem                                    ;;  
+;;;;;;;;;;;;;;;;;;;;;;;;;                         ;;
+;;; INT SIZE CONTROLL ;;;                         ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;                         ;;
+  mov eax, [rsi]      ;;;                         ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;                         ;;
+                                                  ;;
+  cmp eax, -2147483648                            ;;
+  jnb floatToStringNegative                        ;;
+  jmp floatToStringContinue                        ;;
+  floatToStringNegative:                           ;;
+    mov byte [rdi], 45                            ;;
+    inc rdi                                       ;;
+    dec eax                                       ;;
+    not eax                                       ;;
+    jmp floatToStringContinue                      ;;
+                                                  ;;
+  floatToStringContinue:                           ;;
+ ; mov r9d, eax;
+  xor rcx,rcx                                     ;;
+  xor rdx,rdx                                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;          ;;
+  floatToStringLoop:                               ;;
+    div rbx                                       ;;
+    add dl, 48                                    ;;
+    mov byte [rdi+rcx], dl                        ;;
+    inc rcx                                       ;;
+    xor rdx,rdx                                   ;;
+                                                  ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;          ;;
+    cmp al, 0                                     ;;
+    ja floatToStringLoop                           ;;
+;;;;;;;;;;;;;;;;;;;;                              ;;
+;; reverse string ;;                              ;;
+;;;;;;;;;;;;;;;;;;;;                              ;;
+      pop r10                                            ;;
+  floatToStringfindStart:                          ;;
+    cmp rcx, 0                                    ;;
+    je iIntToStringIsZero                         ;;
+    cmp byte [rdi+rcx],48                         ;;
+    jne floatToStringSwitchArea                    ;;
+    dec rcx                                       ;;
+    jmp floatToStringfindStart                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;          ;;
+floatToStringSwitchArea:  
+;;
+  ;set null after rcx+1                           ;;
+  inc rcx                                         ;;
+  mov byte [rdi+rcx], 0                           ;;
+  dec rcx                                         ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;          ;;
+  mov rax, rdi                                    ;;
+  add rdi, rcx                                    ;;
+  call switch                                     ;;
+                                                  ;;
+  jmp floatToStringEnd                              ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;          ;;
+floatToStringIsZero:                               ;;
+  mov byte [rdi+1], 0                             ;;
+  jmp floatToStringEnd                             ;;
+                                                  ;;
+floatToStringEnd:   
+                                            ;;
+  pop rcx                                         ;;
+  pop rax                                         ;;
+ret                                               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
 
 
 
